f02e4a1c8b703d36c81859af195a719f754ad376
vitalii.ivanov <vitalii.ivanov@spryker.com>
2024-01-14 16:54:46 +0200

    Added DependencyProviderMethodNameRule

---
+++ src/Project/Glue/DependencyProviderMethodNameRule.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace ProjectArchitectureSniffer\Project\Glue;
+
+use ArchitectureSniffer\Common\DependencyProvider\AbstractDependencyProviderRule;
+use PHPMD\AbstractNode;
+use PHPMD\Rule\MethodAware;
+
+class DependencyProviderMethodNameRule extends AbstractDependencyProviderRule implements MethodAware
+{
+    /**
+     * @var array
+     */
+    protected $allowedProvideMethodNames = [
+        'provideDependencies',
+    ];
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return static::RULE;
+    }
+
+    /**
+     * @param \PHPMD\AbstractNode $node
+     *
+     * @return void
+     */
+    public function apply(AbstractNode $node)
+    {
+        if (!$this->isDependencyProvider($node, 'Glue')) {
+            return;
+        }
+
+        $this->applyRule($node, $this->allowedProvideMethodNames);
+    }
+}
